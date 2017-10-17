8dc4e72f605382b0c00fe581b26749ebc6329e9c
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-10-17 12:03:39 +0530

    Sign Up page layout.

--- Java Code/src/application/App.java
+++ Java Code/src/application/App.java
@@ -1,10 +1,17 @@
 package application;
 	
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
 
 
@@ -17,6 +24,8 @@ public class App extends Application {
 			Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
 			Scene s = new Scene(root, 800,600);
 			primaryStage.setTitle("IIIT Delhi");
+			Button signup = new Button();
+			signup.setOnAction(new	SignUpEvent());
 			primaryStage.setScene(s);
 			primaryStage.show();
 		} catch(Exception e) {
@@ -26,5 +35,22 @@ public class App extends Application {
 	
 	public static void main(String[] args) {
 		launch(args);
+	}
+	
+	class	SignUpEvent	implements	EventHandler<ActionEvent>	{		
+		@Override	
+		public void	handle(ActionEvent	event)	{	
+				
+		      try{
+		            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("SignUp.fxml"));
+		            Parent root1 = (Parent) fxmlLoader.load();
+		            Stage stage = new Stage();
+		            stage.initModality(Modality.APPLICATION_MODAL);
+		            stage.initStyle(StageStyle.UNDECORATED);
+		            stage.setTitle("IIIT Delhi");
+		            stage.setScene(new Scene(root1));  
+		            stage.show();
+		          }
+		}
 	}
 }
