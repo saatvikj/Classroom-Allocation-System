5effb3e1f7146633cc51f0294d56bcdbff2b1660
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-10-17 11:17:28 +0530

    Container layout fixed.

--- Java Code/src/application/App.java
+++ Java Code/src/application/App.java
@@ -1,7 +1,9 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
@@ -10,10 +12,12 @@ public class App extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
+
+			
+			Parent root = FXMLLoader.load(getClass().getResource("HomePage.fxml"));
+			Scene s = new Scene(root, 800,600);
+			primaryStage.setTitle("IIIT Delhi");
+			primaryStage.setScene(s);
 			primaryStage.show();
 		} catch(Exception e) {
 			e.printStackTrace();
