16a99f587bc687a5d05f66664715bd6719068170
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-10-20 17:32:02 +0530

    Controller separated.

---
+++ Java Code/src/application/BookRoomUI.java
@@ -0,0 +1,35 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class BookRoomUI {
+
+
+	@FXML
+	private void confirmBooking(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("CheckRoomAvail2.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+}
