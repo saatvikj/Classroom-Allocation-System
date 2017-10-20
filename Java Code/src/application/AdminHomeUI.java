3873a7e837b1be384f010d22a715378a0f081dcc
Meghna Gupta <meghnagupta64@gmail.com>
2017-10-20 17:17:35 +0530

    Controller pages seperated (contd.)

---
+++ Java Code/src/application/AdminHomeUI.java
@@ -0,0 +1,111 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+public class AdminHomeUI {
+	
+	@FXML
+	private void viewBookedRooms(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("BookedRoomRecords.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	@FXML
+	private void cancelBooking(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("CancelBooking.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@FXML
+	private void makeBooking(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("CheckRoomAvail1.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@FXML
+	private void handleRequests(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("HandleRequests.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	@FXML
+	private void showNotifications(MouseEvent event) {
+
+		Parent root;
+		try {
+			root = FXMLLoader.load(getClass().getResource("NotificationsUI.fxml"));
+			Stage stage = new Stage();
+			stage.setTitle("IIIT Delhi");
+			stage.setScene(new Scene(root, 800, 600));
+			stage.show();
+
+			((Node) (event.getSource())).getScene().getWindow().hide();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+
+}
