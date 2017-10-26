8519d2f19c18e9a2da137733e8ad705c53ef4006
Meghna Gupta <meghnagupta64@gmail.com>
2017-10-26 12:16:50 +0530

    SignUp.java added

--- Java Code/src/backend/SignUp.java
+++ Java Code/src/backend/SignUp.java
@@ -1,5 +1,8 @@
 package backend;
 
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.ArrayList;
 
 public class SignUp {
@@ -7,19 +10,10 @@ public class SignUp {
 	private String name;
 	private String emailID;
 	private String typeOfUser;
-	private String passwword;
+	private String password;
 	private String confirmPassword;
 	private ArrayList<User> listOfUsers;
 	private ArrayList<Course> allCourses;
-
-	public boolean validateEmail() throws InvalidEmailException {
-		/*
-		 * It checks whether the email ID is a valid IIIT Delhi email ID or not.
-		 * Check after @ it is iiitd.ac.in or not (Substring) Returns true if it
-		 * is a valid email ID, else returns false
-		 */
-		return true;
-	}
 
 	public String getName() {
 		return name;
@@ -45,12 +39,12 @@ public class SignUp {
 		this.typeOfUser = typeOfUser;
 	}
 
-	public String getPasswword() {
-		return passwword;
+	public String getPassword() {
+		return password;
 	}
 
-	public void setPasswword(String passwword) {
-		this.passwword = passwword;
+	public void setPasswword(String password) {
+		this.password = password;
 	}
 
 	public String getConfirmPassword() {
@@ -77,13 +71,60 @@ public class SignUp {
 		this.allCourses = allCourses;
 	}
 
+	public boolean validateEmail() throws InvalidEmailException {
+		/*
+		 * It checks whether the email ID is a valid IIIT Delhi email ID or not.
+		 * Check after @ it is iiitd.ac.in or not (Substring) Returns true if it
+		 * is a valid email ID, else returns false meghna16056@iiitd.ac.in
+		 */
+
+		if (this.getEmailID().split("\\@")[2].split("\\.")[0].equals("iiitd")) {
+			return true;
+		} else {
+			throw new InvalidEmailException("Not a valid IIITD emailID");
+
+		}
+	}
+
 	public boolean checkStrongPassword() throws WeakPasswordException {
 		/*
 		 * It checks whether the entered password is strong enough or not.
 		 * Length >= 8 At Least one digit and at least one alphabet Returns true
 		 * when a password is strong, false when it is not
 		 */
-		return true;
+		
+		String pass = this.password;
+		if(pass.length() >= 8)
+		{
+			boolean flag = false;
+			for(int i = 0; i < pass.length(); i++)
+			{
+				if(Character.isDigit(pass.charAt(i)))
+				{
+					flag = true;
+					break;
+				}
+			}
+			if(flag == true)
+			{
+				boolean check = false;
+				for(int i = 0; i < pass.length(); i++)
+				{
+					if(Character.isUpperCase(pass.charAt(i)) || Character.isLowerCase(pass.charAt(i)))
+					{
+						check = true;
+						break;
+					}
+				}
+				if(check == true)
+				{
+					return true;
+				}
+			}
+				
+		}
+		
+		return false;
 	}
 
 	public String encryptPassword() {
@@ -91,7 +132,30 @@ public class SignUp {
 		 * It encrypts the user’s password (safety feature) Encryption function
 		 * (to be thought of) Returns the encrypted password
 		 */
+		
+		return md5(this.password);
 	}
+	
+	public static String md5(String input) {
+			
+			String md5 = null;
+			if(null == input) return null;
+			try {
+			//Create MessageDigest object for MD5
+			MessageDigest digest = MessageDigest.getInstance("MD5");
+	
+			//Update input string in message digest
+			digest.update(input.getBytes(), 0, input.length());
+	
+			//Converts message digest value in base 16 (hex) 
+			md5 = new BigInteger(1, digest.digest()).toString(16);
+	
+			} catch (NoSuchAlgorithmException e) {
+	
+				e.printStackTrace();
+			}
+			return md5;
+		}
 
 	public void addCoursesToFaculty() {
 		/*
@@ -113,6 +177,7 @@ public class SignUp {
 		 * If confirmed and entered password match, it returns true, else
 		 * returns false
 		 */
+		return (this.getPassword().equals(this.getConfirmPassword()));
 	}
 
 	public void deserializeUsers() {
--- Java Code/src/backend/User.java
+++ Java Code/src/backend/User.java
@@ -4,46 +4,95 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class User {
-	
+
 	protected String name;
 	protected String emailID;
 	protected String encryptedPassword;
 	protected Map<Slot, ClassRoom> bookedRooms;
 	public ArrayList<String> listOfNotifications;
-	
-	public void viewRoomBooked()
-	{
+
+	public User(String name, String emailID, String encryptedPassword) {
+		super();
+		this.name = name;
+		this.emailID = emailID;
+		this.encryptedPassword = encryptedPassword;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmailID() {
+		return emailID;
+	}
+
+	public void setEmailID(String emailID) {
+		this.emailID = emailID;
+	}
+
+	public String getEncryptedPassword() {
+		return encryptedPassword;
+	}
+
+	public void setEncryptedPassword(String encryptedPassword) {
+		this.encryptedPassword = encryptedPassword;
+	}
+
+	public Map<Slot, ClassRoom> getBookedRooms() {
+		return bookedRooms;
+	}
+
+	public void setBookedRooms(Map<Slot, ClassRoom> bookedRooms) {
+		this.bookedRooms = bookedRooms;
+	}
+
+	public ArrayList<String> getListOfNotifications() {
+		return listOfNotifications;
+	}
+
+	public void setListOfNotifications(ArrayList<String> listOfNotifications) {
+		this.listOfNotifications = listOfNotifications;
+	}
+
+	public void viewRoomBooked() {
 		/*
-		 * Iterates through the hashmap and prints all the rooms booked by that User, else prints a fancy message of no bookings.
+		 * Iterates through the hashmap and prints all the rooms booked by that
+		 * User, else prints a fancy message of no bookings.
 		 */
 	}
-	
-	public boolean checkRoomAvailability(ClassRoom reqRoom, Slot reqSlot)
-	{
+
+	public boolean checkRoomAvailability(ClassRoom reqRoom, Slot reqSlot) {
 		/*
-		 * It directly calls the method checkIfEmptyInSlot of the reqRoom object with reqSlot as the parameter.
+		 * It directly calls the method checkIfEmptyInSlot of the reqRoom object
+		 * with reqSlot as the parameter.
 		 */
 		return true;
 	}
-	
-	public void makeBooking(ClassRoom reqRoom, Slot reqSlot)
-	{
+
+	public void makeBooking(ClassRoom reqRoom, Slot reqSlot) {
 		/*
-		 * It calls the method makeBooking of the reqRoom object with reqSlot as the first parameter rest is handled by inheritance (comment).
+		 * It calls the method makeBooking of the reqRoom object with reqSlot as
+		 * the first parameter rest is handled by inheritance (comment).
 		 */
 	}
-	
-	public void cancelBooking(ClassRoom bookedRoom, Slot bookedSlot)
-	{
+
+	public void cancelBooking(ClassRoom bookedRoom, Slot bookedSlot) {
 		/*
-		 * It calls the method cancelBooking from bookedRoom object with bookedSlot as parameter and then makes this entry in its own hashmap(bookedRooms) as null.
+		 * It calls the method cancelBooking from bookedRoom object with
+		 * bookedSlot as parameter and then makes this entry in its own
+		 * hashmap(bookedRooms) as null.
 		 */
 	}
-	
-	public void populateNotifications()
-	{
+
+	public void populateNotifications() {
 		/*
-		 * It goes through the required things to give notification about and refreshes the listOfNotifications attribute. (New requests for admin, request status for student etc etc)
+		 * It goes through the required things to give notification about and
+		 * refreshes the listOfNotifications attribute. (New requests for admin,
+		 * request status for student etc etc)
 		 */
 	}
 
