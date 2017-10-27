e732299f8cc8c24534508c99d17eb3aa966b1ebb
Saatvik Jain <saatvik16261@iiitd.ac.in>
2017-10-27 13:52:02 +0530

    idk

--- Java Code/src/ui/FacultyHomeUI.java
+++ Java Code/src/ui/FacultyHomeUI.java
@@ -2,17 +2,43 @@ package ui;
 
 import java.io.IOException;
 
+import backend.Faculty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class FacultyHomeUI {
 
+	private Faculty currFaculty;
+
+	@FXML
+	private Label nameFaculty;
+	
+	@FXML
+	private Label emailFaculty;
+	
+	@FXML
+	private Pane main;
+	
+	public void initialize() throws IOException {
+		
+		FXMLLoader ldr = new FXMLLoader(getClass().getResource("/fxml/FacultyHome.fxml"));
+		Parent p = (Parent) ldr.load();
+		currFaculty = (Faculty) p.getUserData();
+		nameFaculty.setText(currFaculty.getName());
+		emailFaculty.setText(currFaculty.getEmailID());
+		
+	}
+	
+
+	
 	@FXML
 	private void showNotifications(MouseEvent event) {
 
